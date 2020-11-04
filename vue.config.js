0b9f0bbb9edf849b239d767c9440f1db9c3d2ef7
Tuomas Ukkola <tuomasukk@gmail.com>
2020-11-04 21:06:50 +0200

    Only use config if in development

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 
-module.exports = {
-  devServer: {
-    https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem")
+let config = {};
+
+if (process.env.NODE_ENV === "development") {
+  config = {
+    devServer: {
+      https: {
+        key: fs.readFileSync("./certs/localhost-key.pem"),
+        cert: fs.readFileSync("./certs/localhost.pem")
+      }
     }
-  }
-};
+  };
+}
+
+module.exports = config;
